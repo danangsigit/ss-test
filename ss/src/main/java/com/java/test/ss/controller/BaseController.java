862afd20ddacfe5e9a046f056ddac7a6f38fa1e1
Danang <danangsigit@gmail.com>
2019-09-14 03:03:09 +0700

    fix feign

--- ss/src/main/java/com/java/test/ss/controller/BaseController.java
+++ ss/src/main/java/com/java/test/ss/controller/BaseController.java
@@ -3,7 +3,6 @@ package com.java.test.ss.controller;
 import com.java.test.ss.commons.exception.ExceptionDTO;
 import com.java.test.ss.commons.rest.AbstractRestController;
 import com.java.test.ss.commons.rest.RestResult;
-import com.java.test.ss.config.FeignClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -22,15 +21,6 @@ public class BaseController extends AbstractRestController implements ErrorContr
     @Override
     public String getErrorPath() {
         return "/error";
-    }
-
-    @ExceptionHandler({FeignClientException.class})
-    @ResponseBody
-    public ResponseEntity<RestResult<Object>> handleRestClientException(FeignClientException exception, HttpServletRequest request) {
-        logException(exception, request);
-        List<String> errors = getRecMsgErrors(exception, null);
-        return createExceptionResponse(HttpStatus.valueOf(exception.getHttpCode()), errors,
-                new ExceptionDTO(exception.getMessage(), "error.status."+exception.getHttpCode(), getTraceId()));
     }
 
 }
